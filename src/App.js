30e8d0a8433f93671b799902b643f4e5146715dc
Aliafify <44806987+Aliafify@users.noreply.github.com>
2022-06-13 16:18:19 +0200

    Students-profiles

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import Students from "./components/Students";
 
-import {Canvas} from 'react-three-fiber'
-import {OrbitControls,Stars} from '@react-three/drei'
- 
-import { Physics, usePlane, useSphere } from "@react-three/cannon"
-function Plane() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
-function Box() {
-  const [ref, api] = useSphere(() => ({ mass: 0, position: [0, 2, 0] }));
-  return (
-    <mesh
-     
-      ref={ref}
-      position={[0, 2, 0]}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="#333" />
-    </mesh>
-  );
-}
 export default function App() {
   return (
-  
-   <Canvas style={{height:'100vh'}}>
-      <OrbitControls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[1, 1, 1]} angle={0.4} />
-      <Physics>
-        <Box />
-        <Plane/>
-      </Physics>
-    </Canvas>
+  <>
+
+  <Students/>
+  </>
+ 
     
   );
 }
